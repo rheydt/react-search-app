e10c9c92b5131a736069b47bc279faa29538bf3b
Rose Heydt <rose.heydt@velir.com>
2017-06-27 17:28:12 -0400

    adding stuff from react frontend starter

---
+++ gulp/gulpfile.js
@@ -0,0 +1,88 @@
+/**
+ *  See ./readme.md for usage, configuration details, and task details.
+**/
+
+// Include gulp and plugins
+const gulp   = require("gulp");
+const quench = require("./quench.js");
+const path   = require("path");
+
+
+// tasks: can run in parallel or in series, see "user supplied keys" in quench.js
+const defaultTasks = ["js", "css", "svg-sprite"];
+
+
+// default configuration
+// For details, see "user supplied keys" in quench.js
+const defaults = {
+    root: path.resolve(__dirname, "../../web/Website/assets"),
+    dest: path.resolve(__dirname, "../../web/Website/assets/build"),
+    tasks: [defaultTasks, "browser-sync"],
+    env: "development", // "development", "production", "local"
+    watch: false
+};
+
+/* watch for single tasks on the command line, eg "gulp js" */
+quench.singleTasks(defaults);
+
+
+/**
+ * development task
+ * Default Task (run when you run 'gulp').
+ */
+gulp.task("default", function(next){
+
+    const config = Object.assign({}, defaults, {
+        env   : "development",
+        watch : true
+    });
+
+    quench.build(config, next);
+
+});
+
+
+/**
+ * production task
+ */
+gulp.task("prod", function(next){
+
+    const config = Object.assign({}, defaults, {
+        env   : "production",
+        watch : false
+    });
+
+    quench.build(config, next);
+
+});
+
+
+/**
+ * build for development without a watcher
+ */
+gulp.task("build", function(next){
+
+    const config = Object.assign({}, defaults, {
+        env   : "development",
+        watch : false
+    });
+
+    quench.build(config, next);
+
+});
+
+
+/**
+ * development task with vm syncing
+ */
+gulp.task("sync", function(next){
+
+    const config = Object.assign({}, defaults, {
+        env   : "development",
+        watch : true,
+        tasks : [defaultTasks, "browser-sync", "vm-sync"]
+    });
+
+    quench.build(config, next);
+
+});
