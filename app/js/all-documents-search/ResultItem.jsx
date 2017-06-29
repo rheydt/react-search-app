ceebca74d191c2b7b8aa2518016c0efc62665526
Rose Heydt <rose.heydt@velir.com>
2017-06-29 14:02:02 -0400

    added ability to render multiple tags

--- app/js/all-documents-search/ResultItem.jsx
+++ app/js/all-documents-search/ResultItem.jsx
@@ -1,20 +1,40 @@
 import React, { Component } from 'react';
-import { object } from 'prop-types';
+import { shape, string, array } from 'prop-types';
 
 
 class ResultsItem extends Component {
 
     static propTypes = {
-        data: object
+        data: shape({
+            title: string,
+            href: string,
+            date: string,
+            tags: array
+        })
+    }
+
+    renderTags = () => {
+
+        const { tags } = this.props.data;
+
+        const tagsList = tags.map((tag, index) =>
+            <a key={index} href="#" className="btn classify">{tag}</a>
+        );
+
+        return (
+            <div className="tags">{tagsList}</div>
+        )
     }
 
     render = () => {
 
+        const { title, href, date } = this.props.data;
+
         return (
             <div className="result-item">
-                <h6>TITLE</h6>
-                <p className="date">DATE</p>
-                <a href="#" className="btn classify">TAG</a>
+                <h6><a href={href}>{title}</a></h6>
+                <p className="date">{date}</p>
+                {this.renderTags()}
             </div>
         );
     }
