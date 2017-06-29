de7f9069a66afd18e08c8387d28b912a2e6136b2
Rose Heydt <rose.heydt@velir.com>
2017-06-29 10:27:44 -0400

    starting to rebuild pagination with functions, adding hardcoded props

--- app/js/all-documents-search/AllDocumentsApp.jsx
+++ app/js/all-documents-search/AllDocumentsApp.jsx
@@ -7,10 +7,22 @@ import Pagination from './Pagination.jsx';
 import Results from './Results.jsx';
 
 class AllDocumentsApp extends Component {
+    state = {
+        currentPage: 1,
+        totalPages: 1000,
+        pageRange: 10
+    }
+
     render = () => {
+        let paginationProps = {
+            currentPage: this.state.currentPage,
+            totalPages: this.state.totalPages,
+            pageRange: this.state.pageRange
+        };
+
         return (
             <div className="all-documents-search">
-                <Pagination />
+                <Pagination {...paginationProps}/>
             </div>
         )
     }
--- app/js/all-documents-search/Pagination.jsx
+++ app/js/all-documents-search/Pagination.jsx
@@ -1,19 +1,71 @@
 import React, { Component } from 'react';
+import { int } from 'prop-types';
+
 
 class Pagination extends Component {
+
+    static propTypes = {
+        currentPage: int,
+        totalPages: int,
+        pageRange: int
+    }
+
+    state =  {
+        showPrev: true,
+        showNext: true
+    }
+
+    renderPrevButton = () => {
+        if (this.state.showPrev) {
+            return (<a href="#" className="prev"></a>);
+        } else {
+            return;
+        }
+    }
+
+    renderNextButton = () => {
+        if (this.state.showNext) {
+            return (<a href="#" className="next"></a>);
+        } else {
+            return;
+        }
+    }
+
+    renderPageXofY = () => {
+        return (
+            <span>Page {this.props.currentPage} of {this.props.totalPages}</span>
+        );
+    }
+
+    renderPageList = () => {
+        return (
+            <span className="pnlGroup">
+                <a href="#">2</a>
+                <a href="#">3</a>
+                <a href="#">4</a>
+                <a href="#">5</a>
+                <a href="#">6</a>
+                <a href="#">7</a>
+                <a href="#">8</a>
+                <a href="#">9</a>
+                <a href="#">10</a>
+            </span>
+        );
+    }
+
 
     render = () => {
         return (
             <div className="pnlResults">
                 <p className="pages">
 
-                    <a href="#" className="previous"></a>
+                    {this.renderPrevButton()}
 
-                    <span>Page: 1</span> of 2664
+                    {this.renderPageXofY()}
 
-                    <span className="pnlGroup"></span>
+                    {this.renderPageList()}
 
-                    <a href="#" className="next"></a>
+                    {this.renderNextButton()}
 
                 </p>
             </div>
