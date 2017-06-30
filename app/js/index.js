d68cf36e2e3d1df56744e5533c93aba7cafcb4e8
Rose Heydt <rose.heydt@velir.com>
2017-06-30 12:06:08 -0400

    removing example news listing app

--- app/js/index.js
+++ app/js/index.js
@@ -4,11 +4,6 @@ import { initAllDocumentsSearch } from "./all-documents-search/index.js";
 
 $(document).ready( function() {
 
-    // News Listing
-    if ($("#js-news-listing-app").length) {
-        initNewsListing($("#js-news-listing-app"));
-    }
-
     // All Documents Search
     if ($("#js-all-documents-search").length) {
         initAllDocumentsSearch($("#js-all-documents-search"));
