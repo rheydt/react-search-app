e6add2e4252549d60b6a3d720aadd08fd4f27dd8
Rose Heydt <rose.heydt@velir.com>
2017-07-05 09:27:56 -0400

    adding component for submit button

---
+++ app/js/all-documents-search/SubmitButton.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { string } from 'prop-types';
+
+
+class SubmitButton extends Component {
+
+    static propTypes = {
+        searchButtonText: searchButtonText
+    }
+
+    render = () => {
+        const { searchButtonText } = this.props;
+
+        return (
+            <button type="submit" className="text">{searchButtonText}</button>
+        );
+    }
+}
+
+export default SubmitButton;
