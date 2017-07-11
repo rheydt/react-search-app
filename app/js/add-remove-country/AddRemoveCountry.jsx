f0e799082c8d62d391a35addd464e8ecc08e8216
Rose Heydt <rose.heydt@velir.com>
2017-07-11 15:10:07 -0400

    beginning to set up add remove to/from country app

---
+++ app/js/add-remove-country/AddRemoveCountry.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from "react-dom";
+
+class AddRemoveCountryApp extends Component {
+
+    render = () => {
+
+        return (
+            <div className="module">
+                Add to or remove from country
+            </div>
+        );
+    }
+}
+export default AddRemoveCountryApp;
--- app/js/index.js
+++ app/js/index.js
@@ -1,5 +1,9 @@
 require('jquery');
+import React, { Component } from 'react';
+import ReactDOM from "react-dom";
+
 import { initAllDocumentsSearch } from "./all-documents-search/index.js";
+import AddRemoveCountryApp from './add-remove-country/AddRemoveCountry.jsx';
 
 
 $(document).ready( function() {
@@ -7,5 +11,15 @@ $(document).ready( function() {
     // All Documents Search
     if ($("#js-all-documents-search").length) {
         initAllDocumentsSearch($("#js-all-documents-search"));
+    }
+
+    // Add to or Remove from Country
+    if ($("#js-add-remove-country").length) {
+        console.log("add or remove country init");
+
+        ReactDOM.render(
+            <AddRemoveCountryApp />,
+            document.getElementById('js-add-remove-country')
+        );
     }
 });
