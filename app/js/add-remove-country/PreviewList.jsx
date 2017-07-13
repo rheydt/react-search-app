01f25d65f1345bc93ec7603ac7080f07efa0040a
Rose Heydt <rose.heydt@velir.com>
2017-07-13 13:09:15 -0400

    dynamic article printing

--- app/js/add-remove-country/PreviewList.jsx
+++ app/js/add-remove-country/PreviewList.jsx
@@ -1,19 +1,50 @@
 import React, { Component } from 'react';
 import ReactDOM from "react-dom";
+import { string, array, func } from 'prop-types';
 
 import Article from './Article.jsx';
 
 
 class PreviewList extends Component {
 
+    static propTypes = {
+        previewLabel: string,
+        previewItems: array,
+        removeButtonText: string,
+        removeFromCountry: func
+    }
+
+    replaceNumToken = (string) => {
+        const { previewItems } = this.props;
+
+        const numPattern = /{num}/g;
+
+        return string.replace(numPattern, previewItems.length);
+    }
+
+    renderPreviewList = () => {
+        const { previewItems, removeButtonText, removeFromCountry } = this.props;
+
+        const removeProps = {
+            text: removeButtonText,
+            method: removeFromCountry
+        }
+
+        return previewItems.map((item, index) =>
+            <Article key={index} data={item} remove={removeProps} />
+        );
+    }
+
     render = () => {
+        const { previewLabel } = this.props;
+
+        const previewLabelWithNum = this.replaceNumToken(previewLabel);
 
         return (
             <div className="articles-preview">
-                <i>Preview</i>
+                <p><i>{previewLabelWithNum}</i></p>
 
-                <Article />
-
+                {this.renderPreviewList()}
             </div>
         );
     }
